3ae368da8f47907d004a9fb612c430e0ef96b2fe
Wieland Schöbl <wulkanat@gmail.com>
2021-10-10 16:56:56 +0200

    feat: adding more components

---
+++ src/app/hypnosis/components/author.component.ts
@@ -0,0 +1,11 @@
+import {Component, Input} from "@angular/core";
+import {Author} from "@wulkanat/hypnothing-core/lib/schema.org";
+
+@Component({
+  selector: 'author',
+  templateUrl: 'author.html',
+  styleUrls: ['author.scss'],
+})
+export class AuthorComponent {
+  @Input() author!: Author;
+}
--- src/app/hypnosis/components/hypnosis-components.module.ts
+++ src/app/hypnosis/components/hypnosis-components.module.ts
@@ -4,13 +4,19 @@ import {MarkdownModule} from "ngx-markdown";
 import {HypnosisSuggestionComponent} from "./hypnosis-suggestion.component";
 import {CreativeWorkComponent} from "./creative-work.component";
 import {MatButtonToggleModule} from "@angular/material/button-toggle";
+import {AuthorComponent} from "./author.component";
+import {MatCardModule} from "@angular/material/card";
+import {MatIconModule} from "@angular/material/icon";
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatChipsModule} from "@angular/material/chips";
+import {MatTableModule} from "@angular/material/table";
 
 /**
  * NOTE: NONE of the components contain the title to remain flexible
  */
 @NgModule({
-  imports: [BrowserModule, MarkdownModule, MatButtonToggleModule],
-  declarations: [HypnosisSuggestionComponent, CreativeWorkComponent],
+  imports: [BrowserModule, MarkdownModule, MatButtonToggleModule, MatCardModule, MatIconModule, MatGridListModule, MatChipsModule, MatTableModule],
+  declarations: [HypnosisSuggestionComponent, CreativeWorkComponent, AuthorComponent],
   exports: [
     HypnosisSuggestionComponent
   ]
